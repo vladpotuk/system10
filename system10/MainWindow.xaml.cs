b45aca409f6370d59fc314636bdba4468736cad3
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-09 14:10:16 +0300

    update

--- system10/MainWindow.xaml.cs
+++ system10/MainWindow.xaml.cs
@@ -1,24 +1,38 @@
-﻿using System.Text;
+﻿using System;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace system10
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        
+        private static Semaphore semaphore = new Semaphore(3, 3, "LimitedInstancesAppSemaphore");
+
         public MainWindow()
         {
+            if (!semaphore.WaitOne(TimeSpan.Zero))
+            {
+                
+                MessageBox.Show("Додаток вже запущено в трьох копіях. Нова копія не може бути запущена.", "Інформація", MessageBoxButton.OK, MessageBoxImage.Information);
+                Application.Current.Shutdown();
+                return;
+            }
+
             InitializeComponent();
+            this.Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            
+            semaphore.Release();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            this.Close();
         }
     }
 }
